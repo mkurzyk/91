f8a816e26cd529e7d72a067626ce1ed282f221df
Mariusz Kurzyk <mariusz.kurzyk@gmail.com>
2018-04-06 23:15:43 +0200

    add code js

---
+++ js/trianglearea.js
@@ -0,0 +1,14 @@
+
+function getTriangleArea (a,h) {
+	if ((a <= 0) || (h <= 0)) {
+    console.log('Wrong data');
+} else {
+	return wynik = (a*h/2);
+	}
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(5, 20);
+var triangle3Area = getTriangleArea(15, 35);
